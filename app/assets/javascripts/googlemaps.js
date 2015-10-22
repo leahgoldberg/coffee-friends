7606c500facb98193fcd89139f5f63bc91727c0c
leahgoldberg <leahgoldberg31@gmail.com>
2015-10-22 12:45:23 -0400

    refactor js files

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -1,18 +1,15 @@
 function initMap() {
   var geocoder = new google.maps.Geocoder();
   var address = document.getElementById("address").value;
-  var mapOptions = {
-    zoom: 16,
-    scrollwheel: false
-  }
+  var mapOptions = {zoom: 16, scrollwheel: false};
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    geocoder.geocode( { 'address': address}, function(results, status) {
+  geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-          map: map,
-          position: map.center
+        map: map,
+        position: map.center
       });
     } else {
       alert("Geocode was not successful for the following reason: " + status);
