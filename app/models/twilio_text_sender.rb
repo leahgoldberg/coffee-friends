a193a10836edd3ae56cd2bfeaadfd742b03f64b9
leahgoldberg <leahgoldberg31@gmail.com>
2016-01-31 14:33:02 -0500

    Refactor twilio text sender as a class instead of module

--- app/models/twilio_text_sender.rb
+++ app/models/twilio_text_sender.rb
@@ -1,52 +1,50 @@
-module TwilioTextSender
+class TwilioTextSender
 
-	extend self
+	def initialize(coffee_gift)
+		@coffee_gift = coffee_gift
+		@account_sid = ENV["twilio_account_sid"]
+		@auth_token = ENV["twilio_auth_token"]
+	end
+
+	def send!
+		@coffee_gift.redeemed ? send_text(redeem_message) : send_text(receive_message)
+	end
+
+	private
 
 	def configure_client
-		account_sid = ENV["twilio_account_sid"]
-		auth_token = ENV["twilio_auth_token"]
-
 		Twilio.configure do |config|
-		  config.account_sid = account_sid
-		  config.auth_token = auth_token
+			config.account_sid = @account_sid
+			config.auth_token = @auth_token
 		end
-
 		@client = Twilio::REST::Client.new
 	end
 
-	def send!(coffee_gift)
-		if coffee_gift.redeemed
-			return send_text(coffee_gift, redeem_message)
-		else
-			return send_text(coffee_gift, receive_message(coffee_gift))
-		end
-	end
-
-	def send_text(coffee_gift, text_body)
+	def send_text(text_body)
 		configure_client
 		begin
 			@client.account.messages.create({
 				from: ENV["twilio_phone"],
-				to: coffee_gift.phone,
+				to: @coffee_gift.phone,
 				body: text_body
-			})
-			return nil
-		rescue Twilio::REST::RequestError => e
-			puts "ERROR: #{e.message}"
-			return "Sorry! Something went wrong on our end and we were unable to send a notification SMS. Please let your friend know where to pick up their coffee!"
+				})
+				return nil
+			rescue Twilio::REST::RequestError => e
+				puts "ERROR: #{e.message}"
+				return "Sorry! Something went wrong on our end and we were unable to send a notification SMS. Please let your friend know where to pick up their coffee!"
+			end
 		end
-	end
 
-	def receive_message(coffee_gift)
-		if coffee_gift.message.length > 1
-			"You received a coffee at #{coffee_gift.cafe.name} from #{coffee_gift.giver.first_name} with the message: \"#{coffee_gift.message}\" Visit http://coffeefriends.herokuapp.com to redeem."
-		else
-			"You received a coffee at #{coffee_gift.cafe.name} from #{coffee_gift.giver.first_name}! Visit http://coffeefriends.herokuapp.com to redeem."
-		end	
-	end
+		def receive_message
+			if @coffee_gift.message.length > 1
+				"You received a coffee at #{@coffee_gift.cafe.name} from #{@coffee_gift.giver.first_name} with the message: \"#{@coffee_gift.message}\" Visit http://coffeefriends.herokuapp.com to redeem."
+			else
+				"You received a coffee at #{@coffee_gift.cafe.name} from #{@coffee_gift.giver.first_name}! Visit http://coffeefriends.herokuapp.com to redeem."
+			end
+		end
 
-	def redeem_message
-		"Your coffee has been redeemed! Do something nice and maybe someone will buy you another one."
-	end
+		def redeem_message
+			"Your coffee has been redeemed! Do something nice and maybe someone will buy you another one."
+		end
 
 end
