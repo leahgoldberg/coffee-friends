0d8923db17088fe810e789fa76af79fde1a63768
Benjamin Ellis <ellisbe@gmail.com>
2016-02-27 12:27:12 -0500

    added page total to cafe view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -23,7 +23,37 @@ $(document).ready(function() {
   }
 
   if ($('#cafe_gifts_table').length) {
-    $('#cafe_gifts_table').DataTable();
+    $('#cafe_gifts_table').DataTable({
+      "footerCallback": function ( row, data, start, end, display ) {
+
+        var api = this.api(), data;
+        
+        var intVal = function ( i ) {
+          return typeof i === 'string' ?
+          i.replace(/[\$,]/g, '')*1 :
+          typeof i === 'number' ?
+          i : 0;
+        };
+        total = api
+        .column( 4 )
+        .data()
+        .reduce( function (a, b) {
+          return intVal(a) + intVal(b);
+        }, 0 );
+
+        pageTotal = api
+        .column( 4, { page: 'current'} )
+        .data()
+        .reduce( function (a, b) {
+          return intVal(a) + intVal(b);
+        }, 0 );
+
+
+        $(api.column(4).footer() ).html(
+          '$' + Number(pageTotal).toFixed(2)
+          );
+      }
+    });
   }
 
 });
