c7ab598129a8fdd782fc4ea33c5ef96e9cc0847c
Benjamin Ellis <ellisbe@gmail.com>
2016-03-26 12:01:11 -0400

    cafe markers can now have click events.

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -19,25 +19,43 @@ function initMap() {
 };
 
 function initMapOfCafes() {
-  var geocoder = new google.maps.Geocoder();
   var center = new google.maps.LatLng(40.712239,-73.957136);
   var mapOptions = {
     'zoom': 12,
     'center': center,
     'mapTypeId': google.maps.MapTypeId.ROADMAP
   };
-
-  var map = new google.maps.Map(document.getElementById("mapOfCafes"), mapOptions);
   
+  //global map element
+  map = new google.maps.Map(document.getElementById("mapOfCafes"), mapOptions);
+
   for (var i = 0; i < gon.cafes.length; i++){
-    geocoder.geocode({ 'address': gon.cafes[i].address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        var latLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-        cafeMarker = new google.maps.Marker({'position': latLng, 'map': map});
-      } 
-      else {
-        console.log("Geocode was not successful for the following reason: " + status);
-      }
-    });
+    addCafeMarker(gon.cafes[i]);
   }
-};
+
+}
+function addCafeMarker(cafe){
+  var geocoder = new google.maps.Geocoder();
+  console.log(cafe);
+  geocoder.geocode({ 'address': cafe.address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+      cafeMarker = new google.maps.Marker({'position': latLng, 'map': map});
+      attachCafeInfo(cafeMarker, cafe);
+    } 
+    else {
+      console.log("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+function attachCafeInfo(marker, cafe) {
+  var infowindow = new google.maps.InfoWindow({
+    content: cafe.id.toString()
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(marker.get('map'), marker);
+  });
+}
+
