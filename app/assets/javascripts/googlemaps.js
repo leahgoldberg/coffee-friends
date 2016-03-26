cc1f992e3ceaff816243e57f2867c3c4b0ffb00e
Benjamin Ellis <ellisbe@gmail.com>
2016-03-26 17:44:51 -0400

    map pans to markers if you click on cafe list.

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -34,16 +34,20 @@ function initMapOfCafes() {
   for (var i = 0; i < gon.cafes.length; i++){
     addCafeMarker(gon.cafes[i]);
   }
-
 }
+
 function addCafeMarker(cafe){
+  //global markerlist
+  markerList = [];
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode({ 'address': cafe.address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       var latLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
       cafeMarker = new google.maps.Marker({'position': latLng, 'map': map});
       cafeMarker.cafeInfo = cafe;
+      cafeMarker.cafeInfo.divID = "#cafe_div_id_" + cafe.id;
       attachCafeInfo(cafeMarker, cafe);
+      markerList.push(cafeMarker);
     } 
     else {
       console.log("Geocode was not successful for the following reason: " + status);
@@ -52,15 +56,43 @@ function addCafeMarker(cafe){
 }
 
 function attachCafeInfo(marker, cafe) {
-  console.log(marker.cafeInfo.name);
-  marker.addListener('click', function() {
-    infowindow.setContent(marker.cafeInfo.name);
-    infowindow.open(marker.get('map'), marker);
-    var divID = '#cafe_div_id_' + marker.cafeInfo.id;
-    $(".cafe_menu").addClass('hide');
-    $(divID + " .cafe_menu").removeClass('hide'); 
-    window.location.href = divID; 
-    map.panTo(marker.getPosition());
+    marker.addListener('click', function(e) {
+      infowindow.setContent(marker.cafeInfo.name);
+      infowindow.open(marker.get('map'), marker);
+      var divID = '#cafe_div_id_' + marker.cafeInfo.id;
+      $(".cafe_menu").addClass('hide');
+      $(divID + " .cafe_menu").removeClass('hide'); 
+      // $('.cafes-list-view').animate({
+      //     scrollTop: $(divID).offset().top-100
+      // }, 2000);
+
+      window.location.href = divID; 
+      map.panTo(marker.getPosition());
   });
 }
 
+function setClickListenersToCafes(){
+  
+  $(".cafe_list_div").click(function(e){
+    divID = "#"+($(this).attr('id'));
+    $(".cafe_menu").addClass('hide');
+    $(divID + " .cafe_menu").removeClass('hide'); 
+    window.location.href = divID;
+    for(var i=0; i<markerList.length; i++){    
+      if(markerList[i].cafeInfo.divID == divID){
+        map.panTo(markerList[i].getPosition());
+        infowindow.setContent(markerList[i].cafeInfo.name);
+        infowindow.open(markerList[i].get('map'), markerList[i]);
+
+      }
+    }
+      // infowindow.setContent(marker.cafeInfo.name);
+      // infowindow.open(marker.get('map'), marker);
+      // var divID = '#cafe_div_id_' + marker.cafeInfo.id;
+      // $(".cafe_menu").addClass('hide');
+      // $(divID + " .cafe_menu").removeClass('hide'); 
+      // window.location.href = divID; 
+      // map.panTo(marker.getPosition());
+
+  }); 
+}
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   
   if ($('#mapOfCafes').length) {
     initMapOfCafes();
+    setClickListenersToCafes();
   }
 
   if ($('#map').length) {
